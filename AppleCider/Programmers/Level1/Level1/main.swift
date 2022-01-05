1b06077d5b6ffe1a85af82078af5dbb213283f55
just1103 <hyojuson1103@gmail.com>
2022-01-05 15:37:12 +0900

    programmers: Level1 별찍기 문제

---
+++ AppleCider/Programmers/Level1/Level1/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Level1
+//
+//  Created by Hyoju Son on 2022/01/05.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ AppleCider/Programmers/Level1/Level1/별찍기.swift
@@ -0,0 +1,8 @@
+//
+//  별찍기.swift
+//  Level1
+//
+//  Created by Hyoju Son on 2022/01/05.
+//
+
+import Foundation
