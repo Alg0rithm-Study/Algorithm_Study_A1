cd7c1f3e992f94f3b69d1f81f6bf06cf546d46d1
yanghojoon <yanghojoon8487@gmail.com>
2022-01-09 23:44:18 +0900

    leetcode: Build Array From Permutation

---
+++ Hodaeng/LeetCode/LeetCode/LeetCode/Array/BuildArrayFromPermutation.swift
@@ -0,0 +1,20 @@
+//
+//  BuildArrayFromPermutation.swift
+//  LeetCode
+//
+//  Created by 양호준 on 2022/01/09.
+//
+
+import Foundation
+
+class Solution {
+    func buildArray(_ nums: [Int]) -> [Int] {
+        let length = nums.count
+        var array: [Int] = []
+        (0...length - 1).forEach { index in
+            array.append(nums[nums[index]])
+        }
+        
+        return array
+    }
+}
