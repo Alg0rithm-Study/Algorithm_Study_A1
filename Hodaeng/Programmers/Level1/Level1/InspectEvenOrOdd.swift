d80b060197b87495cc27b743f613bf833c9ea5df
yanghojoon <yanghojoon8487@gmail.com>
2022-01-25 00:05:06 +0900

    programmers: 짝수와 홀수

---
+++ Hodaeng/Programmers/Level1/Level1/InspectEvenOrOdd.swift
@@ -0,0 +1,16 @@
+//
+//  InspectEvenOrOdd.swift
+//  Level1
+//
+//  Created by 양호준 on 2022/01/25.
+//
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    if num % 2 == 0 {
+        return "Even"
+    } else {
+        return "Odd"
+    }
+}
